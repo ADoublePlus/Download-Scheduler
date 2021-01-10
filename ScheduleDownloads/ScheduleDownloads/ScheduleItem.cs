271485069df2abc813afc6fd9c4d2eec8b4970fd
John Wonson <jmpwonson@gmail.com>
2021-01-10 16:40:14 +1100

    Create ScheduleItem.cs

---
+++ ScheduleDownloads/ScheduleDownloads/ScheduleItem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ScheduleDownloads
+{
+    public enum StatusType
+    {
+        CREATING,
+        WAITING,
+        RUNNING,
+        COMPLETED
+    }
+
+    abstract public class ScheduleItem
+    {
+        public StatusType Status { get; set; }
+        public DateTime? TargetDate { get; set; }
+
+        public Runner Runner { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        public string LocalName { get; set; }
+
+        private long id;
+
+        public long Id
+        {
+            get { return id; }
+
+            protected set
+            {
+                id = value;
+            }
+        }
+
+        public ScheduleItem()
+        {
+            InitialFields();
+        }
+
+        public void InitialFields()
+        {
+            ResourceType = ResourceType.UNKNOWN;
+            TargetDate = DateTime.Now;
+            Status = StatusType.CREATING;
+        }
+
+        public abstract Runner GetRunner();
+
+        public abstract override string ToString();
+
+        protected string GetStatusString()
+        {
+            string s;
+
+            switch (Status)
+            {
+                case StatusType.CREATING:
+                    s = "CREATING";
+                    break;
+
+                case StatusType.WAITING:
+                    s = "WAITING";
+                    break;
+
+                case StatusType.RUNNING:
+                    s = "RUNNING";
+                    break;
+
+                case StatusType.COMPLETED:
+                    s = "COMPLETED";
+                    break;
+
+                default:
+                    s = "UNKNOWN";
+                    break;
+            }
+
+            return s;
+        }
+    }
+}

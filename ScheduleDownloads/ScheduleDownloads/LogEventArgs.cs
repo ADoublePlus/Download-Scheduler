f44f68df305ba7b22957ce4d055fb76b25842378
John Wonson <jmpwonson@gmail.com>
2021-01-10 16:04:28 +1100

    Create LogEventArgs.cs

---
+++ ScheduleDownloads/ScheduleDownloads/LogEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScheduleDownloads
+{
+    public class LogEventArgs : EventArgs
+    {
+        public string Text { get; private set; }
+
+        public LogEventArgs(string s)
+        {
+            Text = s;
+        }
+    }
+}

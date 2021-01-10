766b1ce76b41cdc7e866bb5049f52d65341b070c
John Wonson <jmpwonson@gmail.com>
2021-01-10 16:22:10 +1100

    Create AppSettings.cs

---
+++ ScheduleDownloads/ScheduleDownloads/AppSettings.cs
@@ -0,0 +1,49 @@
+﻿namespace ScheduleDownloads
+{
+    abstract public class AppSettings
+    {
+        protected bool dirty;
+
+        protected string defaultDirectory;
+
+        public string DefaultDirectory
+        {
+            get { return defaultDirectory; }
+
+            set
+            {
+                defaultDirectory = value;
+
+                dirty = true;
+            }
+        }
+
+        public AppSettings() { }
+
+        ~AppSettings()
+        {
+            if (dirty)
+            {
+                Save();
+            }
+        }
+
+        public void Load()
+        {
+            Read();
+
+            dirty = false;
+        }
+
+        protected abstract void Read();
+
+        public void Save()
+        {
+            Write();
+
+            dirty = false;
+        }
+
+        protected abstract void Write();
+    }
+}

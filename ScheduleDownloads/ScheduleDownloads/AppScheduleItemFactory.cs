fc347b2ba23ce49579413c80deb03b3d85725932
John Wonson <jmpwonson@gmail.com>
2021-01-10 17:12:57 +1100

    Create AppScheduleItemFactory.cs

---
+++ ScheduleDownloads/ScheduleDownloads/AppScheduleItemFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace ScheduleDownloads
+{
+    class AppScheduleItemFactory : ScheduleItemFactory
+    {
+        public override ScheduleItem GetScheduleItem(ResourceType rt)
+        {
+            ScheduleItem si = null;
+
+            switch (rt)
+            {
+                case ResourceType.WEB:
+                    si = new WebScheduleItem();
+                    break;
+
+                case ResourceType.FTP:
+                    si = new FtpScheduleItem();
+                    break;
+
+                default:
+                    break;
+            }
+
+            si.ResourceType = rt;
+
+            return si;
+        }
+
+        public AppScheduleItemFactory() : base() { }
+    }
+}

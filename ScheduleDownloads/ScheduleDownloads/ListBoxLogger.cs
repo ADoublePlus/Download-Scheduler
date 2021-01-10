eec10b7124ae736abce2addc0cabb2b0fbdce20f
John Wonson <jmpwonson@gmail.com>
2021-01-10 16:02:35 +1100

    Create ListBoxLogger.cs

---
+++ ScheduleDownloads/ScheduleDownloads/ListBoxLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ScheduleDownloads
+{
+    class ListBoxLogger : ILogger
+    {
+        ListBox listBox;
+
+        public ListBoxLogger(ListBox listBox)
+        {
+            this.listBox = listBox;
+        }
+
+        public void LogMessage(string msg)
+        {
+            MethodInvoker logDelegate = delegate
+            {
+                listBox.Items.Add(String.Format("{0:yyyy-MM-dd} {0:hh:mm:ss tt} - ", DateTime.Now) + msg);
+            };
+
+            if (listBox.InvokeRequired)
+            {
+                listBox.Invoke(logDelegate);
+            }
+            else
+            {
+                logDelegate();
+            }
+        }
+    }
+}

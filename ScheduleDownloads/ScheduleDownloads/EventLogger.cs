88eec6f64a05c462ef2a5d7103c95a6422da7dba
John Wonson <jmpwonson@gmail.com>
2021-01-10 16:17:16 +1100

    Create EventLogger.cs

---
+++ ScheduleDownloads/ScheduleDownloads/EventLogger.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace ScheduleDownloads
+{
+    class EventLogger : ILogger
+    {
+        public EventLogger() { }
+
+        public void LogMessage(string msg)
+        {
+            EventLog.WriteEntry("Logger", msg);
+        }
+    }
+}
